936e24cfb1e537bf0d01b7844ac6940d5c92ec4e
Ahmed Mahsook <163556739+ahmedmahsook@users.noreply.github.com>
2026-04-25 22:01:54 +0530

    Time: 0 ms (100.00%) | Memory: 4.1 MB (62.62%) - LeetSync

---
+++ 69-sqrtx/sqrtx.go
@@ -0,0 +1,19 @@
+func mySqrt(x int) int {
+    left, right := 0, x
+    ans := 0
+
+    for left <= right {
+        mid := left + (right-left)/2
+
+        if mid*mid == x {
+            return mid
+        } else if mid*mid < x {
+            ans = mid
+            left = mid + 1
+        } else {
+            right = mid - 1
+        }
+    }
+
+    return ans
+}
