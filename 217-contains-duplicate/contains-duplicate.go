fe3ddc805b762572a26218f75e42514ccd355438
Ahmed Mahsook <163556739+ahmedmahsook@users.noreply.github.com>
2026-04-28 07:48:58 +0530

    Time: 17 ms (79.43%) | Memory: 11.5 MB (51.94%) - LeetSync

--- 217-contains-duplicate/contains-duplicate.go
+++ 217-contains-duplicate/contains-duplicate.go
@@ -1,11 +1,10 @@
 func containsDuplicate(nums []int) bool {
     seen:=make(map[int]bool)
-   for _,num:=range nums{
-
-    if seen[num]{
-        return true
+    for _,num:=range nums{
+        if seen[num]{
+            return true
     }
     seen[num]=true
-   }
+    }
     return false
 }
