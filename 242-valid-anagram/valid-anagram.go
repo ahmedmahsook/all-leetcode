c94dd4bbef89cd363bac7d86d8f1c0e6b357f144
Ahmed Mahsook <163556739+ahmedmahsook@users.noreply.github.com>
2026-04-30 08:40:03 +0530

    Time: 5 ms (39.75%) | Memory: 4.7 MB (58.28%) - LeetSync

--- 242-valid-anagram/valid-anagram.go
+++ 242-valid-anagram/valid-anagram.go
@@ -1,22 +1,20 @@
 func isAnagram(s string, t string) bool {
-    sCount := make(map[rune]int)
-    tCount := make(map[rune]int)
-
-    if len(s) != len(t) {
-    return false
-}
-    
-    for _,ch:=range s{
-        sCount[ch]++
-    }
-    for _,ch:=range t{
-        tCount[ch]++
-    }
-    for ch, count := range sCount {
-    if tCount[ch] != count {
+    sCount:=make(map[rune]int)
+    tCount:=make(map[rune]int)
+    if len(s)!=len(t){
         return false
     }
-}
-return true
-    
+
+     for _,ch:=range s{
+        sCount[ch]++
+     }
+     for _,ch:=range t{
+        tCount[ch]++
+     }
+     for ch,count:=range sCount{
+      if tCount[ch]!=count{
+        return false
+      }
+     }
+    return true
 }
